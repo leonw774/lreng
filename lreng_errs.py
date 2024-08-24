486918985800ed8d81e619fc1b57a8c5508496a1
leonw774 <leonw774@gmail.com>
2024-08-24 19:02:28 +0800

    Update lreng_errs.py

--- lreng_errs.py
+++ lreng_errs.py
@@ -5,7 +5,6 @@ def throw_syntax_err_msg(pos: tuple[int, int], msg: str):
 
 def print_semantic_err_msg(pos: tuple[int, int], msg: str):
     print(f'[SemanticError]: Line {pos[0]} col {pos[1]}: {msg}')
-    exit(1)
 
 def throw_runtime_err_msg(
         db_info: tuple[int, int] | tuple[str] | None,
